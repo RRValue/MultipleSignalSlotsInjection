899fb10c2970358714fb0987add6fc6eb1cbfd3c
Ronti <nasowasaberauch@posteo.de>
2022-04-19 15:37:46 +0200

    finished setup

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,10 @@
+#include <QtCore/QObject>
+#include <QtWidgets/QWidget>
+
 int main(int argc, char* argv[])
 {
+    const auto* obj = new QObject();
+    const auto* wdg = new QWidget();
+
     return 0;
 }
